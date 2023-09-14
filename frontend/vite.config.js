6fdf56517e2c08db16fe12aff3869e5122fba147
kchan <kwunfuchan@gmail.com>
2023-09-14 19:54:46 +0100

    [testing] sixth attempt for hosting - part.3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite';
-import { splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        splitVendorChunkPlugin()
     ],
     server: {
         host: '127.0.0.1',
         port: 3000,
     },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
 });
