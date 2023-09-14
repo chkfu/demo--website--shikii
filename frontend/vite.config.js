4f3386170528bb0edaeff2daf8e23666b110951e
kchan <kwunfuchan@gmail.com>
2023-09-14 19:45:18 +0100

    [testing] sixth attempt for hosting (chunck larger than 500Kb)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        chunkSplitPlugin()
+    ],
     server: {
         host: '127.0.0.1',
         port: 3000,
